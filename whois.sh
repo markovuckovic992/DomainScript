dac2f1663d1d030d4c2b0213ba3a4a04d21a8c36
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-30 17:23:00 +0100

    fix

--- whois.sh
+++ whois.sh
@@ -7,6 +7,10 @@ fi
 
 for domain in `cat domains.txt`
 do
-   echo $domain, $(whois $domain | grep 'Registrant Email:' | awk 'BEGIN{FS=" "}{print $3}') >> whois-results.csv
+	{ 
+		echo $domain, $(whois $domain | grep 'Registrant Email:' | awk 'BEGIN{FS=" "}{print $3}') >> whois-results.csv
+	} || {
+		continue
+	}
 done
 
