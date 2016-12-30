14f733eedd3ea4859d35235786102cdde14763d7
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-30 17:16:21 +0100

    fix

--- whois.sh
+++ whois.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+file="whois-results.csv"
+
+if [ -f $file ] ; then
+    rm $file
+fi
+
 for domain in `cat domains.txt`
 do
-   echo $domain $(whois $domain | grep 'Registrant Email:' | awk 'BEGIN{FS=" "}{print $3}') >> whois-results.txt
+   echo $domain, $(whois $domain | grep 'Registrant Email:' | awk 'BEGIN{FS=" "}{print $3}') >> whois-results.csv
 done
 
