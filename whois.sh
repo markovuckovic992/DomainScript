644176e801e4323729f539adceb5bc646b374d96
markovuckovic992 <markovuckovic992@yahoo.com>
2016-12-30 15:37:23 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- whois.sh
+++ whois.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for domain in `cat domains.txt`
 do
-   echo $domain
-   `whois $domain >> whois-results.txt`
+   echo $domain $(whois $domain | grep 'Registrant Email:' | awk 'BEGIN{FS=" "}{print $3}') >> whois-results.txt
 done
+
