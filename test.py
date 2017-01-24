2df6b1fb1dfcb3895b26384fe1b289de1ca74269
markovuckovic992 <markovuckovic992@yahoo.com>
2017-01-24 08:59:09 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- test.py
+++ test.py
@@ -1,37 +1,19 @@
 # from django.core.mail import send_mail
 from django.conf import settings
-from smtplib import SMTPServerDisconnected
 import os, django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
 django.setup()
+from domain.models import RawLeads
+from datetime import datetime
 
-# send_mail(
-#     'Test',  # Title
-#     'Test',  # Body
-#     settings.EMAIL_HOST_USER,
-#     ['markovuckovic992@yahoo.com'],
-#     fail_silently=False,
-#     html_message='<h1>Test</h1>',
-#     reply_to=("edomainexpert@gmail.com", ),
-# )
-from django.core import mail
 
-connection = mail.get_connection()
-connection.open()
-emails = []
-
-email = mail.EmailMultiAlternatives(
-    'Test',
-    'Test',
-    'Web Domain Expert <' + settings.EMAIL_HOST_USER + '>',
-    ['markovuckovic992@yahoo.com'],
-    reply_to=("edomainexpert@gmail.com", ),
-    bcc=[bcc_email]
-)
-emails.append(email)
-try:
-    connection.send_messages(emails)
-    raise SMTPServerDisconnected
-except SMTPServerDisconnected:
-    pass
-connection.close()
+with open('convertcsv.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        RawLeads(
+            name_zone=row[0],
+            name_redemption=row[2],
+            date=datetime.now().date(),
+            page=1,
+            activated=1,
+        )
