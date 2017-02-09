211cc2916e3a4b9d174313b3ddc564667d14d969
markovuckovic992 <markovuckovic992@yahoo.com>
2017-02-09 12:03:01 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- test.py
+++ test.py
@@ -3,28 +3,23 @@ from django.conf import settings
 import os, django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
 django.setup()
-from domain.models import RawLeads, BlackList, AllHash
+from domain.models import RawLeads, Emails
 from datetime import datetime
 import csv
 from operator import attrgetter
 
+leads = RawLeads.objects.filter(mail__isnull=False)
 
-# with open('convertcsv.csv', 'r') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#     for row in spamreader:
-#         new = RawLeads(
-#             name_zone=row[1],
-#             name_redemption=row[3],
-#             date=datetime.now().date(),
-#             page=1,
-#             activated=1,
-#             mail=row[4],
-#         )
-#         new.save()
+for lead in leads:
+    mail = "".join(lead.mail.split())
+    mail = mail.replace('Registrant Email:', '')
+    if '@' in mail:
+        RawLeads.objects.filter(id=lead.id).update(mail=mail)
+    else:
+        RawLeads.objects.filter(id=lead.id).update(mail=None)
 
-bls = BlackList.objects.all()
+emails = Emails.objects.all()
 
-for bl in bls:
-    hash_base_ids = map(attrgetter('hash_base_id'), RawLeads.objects.filter(mail=bl.email))
-    AllHash.objects.filter(hash_base_id__in=hash_base_ids)
-    RawLeads.objects.filter(mail=bl.email).delete()
+for email in emails:
+    email = "".join(email.email.split())
+    Emails.objects.filter(id=email.id).update(email=email)
