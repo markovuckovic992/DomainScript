2000515216b859dbf2208f7fa642cc78687184ed
Marko <markovuckovic992@yahoo.com>
2017-02-13 18:59:26 +0100

    links

--- test.py
+++ test.py
@@ -1,25 +1,43 @@
-# from django.core.mail import send_mail
+# # from django.core.mail import send_mail
+# from django.conf import settings
+# import os, django
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
+# django.setup()
+# from domain.models import RawLeads, Emails
+# from datetime import datetime
+# import csv
+# from operator import attrgetter
+
+# leads = RawLeads.objects.filter(mail__isnull=False)
+
+# for lead in leads:
+#     mail = "".join(lead.mail.split())
+#     mail = mail.replace('Registrant Email:', '')
+#     if '@' in mail:
+#         RawLeads.objects.filter(id=lead.id).update(mail=mail)
+#     else:
+#         RawLeads.objects.filter(id=lead.id).update(mail=None)
+
+# emails = Emails.objects.all()
+
+# for email in emails:
+#     tmp = "".join(email.email.split())
+#     Emails.objects.filter(id=email.id).update(email=tmp)
+from django.core import mail
 from django.conf import settings
 import os, django
 os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
 django.setup()
-from domain.models import RawLeads, Emails
-from datetime import datetime
-import csv
-from operator import attrgetter
 
-leads = RawLeads.objects.filter(mail__isnull=False)
-
-for lead in leads:
-    mail = "".join(lead.mail.split())
-    mail = mail.replace('Registrant Email:', '')
-    if '@' in mail:
-        RawLeads.objects.filter(id=lead.id).update(mail=mail)
-    else:
-        RawLeads.objects.filter(id=lead.id).update(mail=None)
-
-emails = Emails.objects.all()
-
-for email in emails:
-    tmp = "".join(email.email.split())
-    Emails.objects.filter(id=email.id).update(email=tmp)
+connection = mail.get_connection()
+connection.open()
+emails = []
+email = mail.EmailMultiAlternatives(
+    'TEST',
+    'TEST',
+    'Web Domain Expert <rtl.ludak@gmail.com>',
+    ['markovuckovic992@yahoo.com'],
+)
+emails.append(email)
+connection.send_messages(emails)
+connection.close()
