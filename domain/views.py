811d2fc3220bc324a12220f59285e3f4ee3b9b11
markovuckovic992 <markovuckovic992@yahoo.com>
2017-03-22 12:48:08 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- domain/views.py
+++ domain/views.py
@@ -715,7 +715,8 @@ def send_pending(request):
             iterator = randint(0, 3)
             link = ('http://www.' + str(hosts[iterator]) + '/offer/?id=' + str(hash_base_id))
             unsubscribe = ('http://www.' + str(hosts[iterator]) + '/unsubscribe/?id=' + str(hash_base_id))
-            case = randint(1, 10)
+            # case = randint(1, 10)
+            case = 1
             msg = eval('form_a_msg' + str(case) + '("' + str(potential_profit.name_redemption) + '","' + str(
                 link) + '","' + str(unsubscribe) + '")')
 
