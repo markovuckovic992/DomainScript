c66a4619136120ed122b39548bd074f194f5351c
markovuckovic992 <markovuckovic992@yahoo.com>
2017-03-24 13:18:41 +0100

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

---
+++ sync.py
@@ -0,0 +1,23 @@
+import django
+import sys
+import traceback
+import os
+from django.core import mail
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
+django.setup()
+from domain.models import RawLeads
+from domain.models import AllHash
+
+offers = RawLeads.objects.all()
+for offer in offers:
+    AllHash(
+        hash_base_id=offer.hash_base_id,
+        date=offer.date,
+    ).save()
+
+offers = AllHash.objects.raw("SELECT * FROM hashes_hzn")
+for offer in offers:
+    AllHash(
+        hash_base_id=offer.hash_base_id,
+        date=offer.date,
+    ).save()
