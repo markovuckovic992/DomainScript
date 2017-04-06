39f00ee93b511defc32632e06ecaca15ba41ec62
markovuckovic992 <markovuckovic992@yahoo.com>
2017-04-06 11:24:03 +0200

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- domain/views.py
+++ domain/views.py
@@ -110,8 +110,8 @@ def runEditing(request):
             script = '_basic_editing'
             language = 'pypy '
         else:
-            script = '_basic_editing'
-            language = 'python '
+            script = 'basic_editing'
+            language = 'pypy '
         com = request.POST['com'].replace('C:\\fakepath\\', '')
         net = request.POST['net'].replace('C:\\fakepath\\', '')
         org = request.POST['org'].replace('C:\\fakepath\\', '')
