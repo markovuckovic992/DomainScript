26930c9ffe4dec666fa78c03f5c11bea8e7dbbc6
Marko <markovuckovic992@yahoo.com>
2017-05-23 23:25:19 +0200

    fixes

--- fix.py
+++ fix.py
@@ -1,4 +1,32 @@
 # from django.core import mail
+from django.conf import settings
+import os, django
+os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
+django.setup()
+from domain.models import RawLeads, DomainException
+
+if __name__ == "__main__":
+    DeletedInfo.objects.filter(reason='')
+
+    deleted = DeletedInfo.objects.get(id=id_)
+    condition = True
+    while condition:
+        try:
+            entry = RawLeads(
+                name_zone=deleted.name_zone,
+                name_redemption=deleted.name_redemption,
+                date=deleted.date,
+                mail=deleted.email,
+                page=1,
+                activated=1,
+                hash_base_id=binascii.hexlify(os.urandom(16))
+            )
+            entry.save()
+            condition = False
+        except:
+            pass
+    DeletedInfo.objects.filter(id=id_).delete()
+    return HttpResponse('{"status": "success"}', content_type="application/json")
 
 
 # connection = mail.get_connection()
@@ -13,40 +41,3 @@
 # emails.append(email)
 # connection.send_messages(emails)
 # connection.close()
-
-# COM 2
-# from django.conf import settings
-# import os, django
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'DomainScript.settings'
-# django.setup()
-# from domain.models import RawLeads, DomainException
-
-# if __name__ == "__main__":
-#     DeletedInfo.objects.filter(reason='')
-
-#     deleted = DeletedInfo.objects.get(id=id_)
-#     condition = True
-#     while condition:
-#         try:
-#             entry = RawLeads(
-#                 name_zone=deleted.name_zone,
-#                 name_redemption=deleted.name_redemption,
-#                 date=deleted.date,
-#                 mail=deleted.email,
-#                 page=1,
-#                 activated=1,
-#                 hash_base_id=binascii.hexlify(os.urandom(16))
-#             )
-#             entry.save()
-#             condition = False
-#         except:
-#             pass
-#     DeletedInfo.objects.filter(id=id_).delete()
-#     return HttpResponse('{"status": "success"}', content_type="application/json")
-# ENDCOM
-import requests
-request = requests.get('http://www.adasd.rsa')
-if request.status_code == 200:
-    print('Web site exists')
-else:
-    print('Web site does not exist') 
