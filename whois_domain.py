ef6684b83e3c0422267cbab6baabdd2c98188232
markovuckovic992 <markovuckovic992@yahoo.com>
2017-05-26 09:18:20 +0200

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- whois_domain.py
+++ whois_domain.py
@@ -44,7 +44,7 @@ def main(date):
                     # response = tube.read()
                 r = requests.get('http://api.whoxy.com/?key=3d28dc0e398efe01dp7caa9f21e7b4fdf&whois=' + data.name_zone + '&format=json')
                 resp_data = r.json()
-                status = resp_data['domain_status']
+                status = resp_data['domain_status'] if 'domain_status' in resp_data.keys() else 0
                 email = resp_data['registrant_contact']['email_address']
                 if ('pendingDelete' in status) or ('redemptionPeriod' in status) or ('No match for' in status):
                     record = DeletedInfo(name_zone=data.name_zone, name_redemption=data.name_redemption, date=data.date, reason='domain has bad status')
