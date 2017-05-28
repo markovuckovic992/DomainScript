ebe197af12d7b5e093953f3cfc661454c972e395
markovuckovic992 <markovuckovic992@yahoo.com>
2017-05-28 11:18:51 +0200

    Signed-off-by: markovuckovic992 <markovuckovic992@yahoo.com>

--- domain/views.py
+++ domain/views.py
@@ -1193,7 +1193,7 @@ def Chart(request):
     succeeded2 = []
 
     master_of_indexes = 1
-    who_analytics = WhoisAnalytics.objects.filter(source="internal")[0:100]
+    who_analytics = WhoisAnalytics.objects.filter(source="internal").order_by('-id')[0:100]
     for entry in who_analytics:
         total.append({
             "x": int(master_of_indexes),
@@ -1207,7 +1207,7 @@ def Chart(request):
         master_of_indexes += 1
 
     master_of_indexes = 1
-    who_analytics = WhoisAnalytics.objects.filter(source="external")[0:100]
+    who_analytics = WhoisAnalytics.objects.filter(source="external").order_by('-id')[0:100]
     for entry in who_analytics:
         total2.append({
             "x": int(master_of_indexes),
